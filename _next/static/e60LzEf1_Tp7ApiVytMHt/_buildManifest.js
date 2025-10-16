72dc7e88171552ac35f943ba53e1db6172341b7b
hunter <hunter@topluck999.com>
2025-10-16 11:20:32 +0800

    Updates

---
+++ _next/static/e60LzEf1_Tp7ApiVytMHt/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/a7f2fb4e51902313.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/panchan.github.io//_next/:path+",
+        "destination": "/panchan.github.io/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
