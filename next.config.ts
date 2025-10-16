1588d79fa0ab087900540296c6b44fee0f2069e0
hunter <hunter@topluck999.com>
2025-10-16 11:58:39 +0800

    feat:修改deploy 命令

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repo = process.env.NEXT_PUBLIC_REPO || 'panchan.github.io';
+const repo = process.env.NEXT_PUBLIC_REPO;
 
 module.exports = {
   // 使 Next.js 输出静态 HTML
   output: 'export',  
 
   // 如果部署在 GitHub Pages 子路径，需要设置 basePath 和 assetPrefix
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
+  basePath: isProd  && repo? `/${repo}` : '',
+  assetPrefix: isProd && repo ? `/${repo}/` : '',
 
   // 如果你用了 next/image，禁用默认图片优化（因为静态导出时服务端优化不可用）
   images: {
